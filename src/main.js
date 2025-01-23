3ef49b14b08c89973223740f34dd504300eeaa6b
ayuarista <aristaadewi8@gmail.com>
2025-01-23 15:11:57 +0800

    add bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+import BootstrapVueNext from 'bootstrap-vue-next';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(BootstrapVueNext);
+
+app.mount('#app');
